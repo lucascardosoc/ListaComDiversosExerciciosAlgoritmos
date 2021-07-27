e9850eefdfaa698bc9218f3d2523838d1de06b3e
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-07-27 12:36:35 -0300

    Exercício 8

--- src/Ex008.java
+++ src/Ex008.java
@@ -6,21 +6,61 @@ public class Ex008 {
 
 		Scanner leitor = new Scanner(System.in);
 		
-		int resposta=0;
-		String sim;
-		int questao1 = 1;
+		String resposta;
 		
+		System.out.println("QUIZ FILME - Saga: Velozes e Furiosos");
+		System.out.println("1- O primeiro filme da saga Velozes e Furiosos foi lancado em 2001?");
+		resposta = leitor.next();
 		
-		while(resposta!=1) {
-			System.out.println("1 - Velozes e furiosos foi lancado em 2001? Digite 1 para sim e 2 para não.");
-			resposta = leitor.nextInt();
+		if(resposta.equalsIgnoreCase("SIM")) {
+		System.out.println("2- O nome do personagem Paul Walker é Dominic Toretto?");
+		resposta = leitor.next();
+		
+		if(resposta.equalsIgnoreCase("NÃO")) {
+		System.out.println("3- O autor Paul Walker faleceu em 2013 em um acidente de carro?");
+		resposta = leitor.next();
+		
+		if(resposta.equalsIgnoreCase("SIM")) {
+		System.out.println("4- Velozes e furiosos 5 se passa no Rio de Janeiro?");
+		resposta = leitor.next();
+		
+		if(resposta.equalsIgnoreCase("SIM")) {
+		System.out.println("5- O nome do personagem Dominic Toretto é Vin Diesel?");
+		resposta = leitor.next();
+		
+		if(resposta.equalsIgnoreCase("SIM")) {
+		System.out.println("6- A saga Velozes e Furiosos tem 10 filmes?");
+		resposta = leitor.next();
+		
+		if(resposta.equalsIgnoreCase("NÃO")) {
+		System.out.println("7- A personagem Mia é casada com o personagem Brian?");
+		resposta = leitor.next();
+		
+		if(resposta.equalsIgnoreCase("SIM")) {
+		System.out.println("8- Velozes e Furiosos 3 se passa nos Estados Unidos?");
+		resposta = leitor.next();
+		
+		if(resposta.equalsIgnoreCase("NÃO")) {
+			System.out.println("9- O número da casa de Dominic Toreto é 1327?");
+			resposta = leitor.next();
+			
+			if(resposta.equalsIgnoreCase("SIM")) {
+			System.out.println("10- Quando o autor Paul Walker morreu estavam gravando o filme de número 7?");	
+			resposta = leitor.next();
+			
+			if(resposta.equalsIgnoreCase("SIM")) {
+			System.out.println("PARABÉNS! VOCÊ ACERTOU TODAS AS PERGUNTAS!");	
+			}
+			}
 		}
-		
-		while(resposta!=2) {
-			System.out.println("2 - Tim Cook foi o criador da Apple? Digite 1 para sim e 2 para não.");
-			resposta = leitor.nextInt();
 		}
-		
+		}
+		}
+		}
+			
+		}
+		}
+		}
 		
 		leitor.close();
 	}
